f9208a1fd8e9100dba5d78694f459a352bd6d399
Adam Gall <adam@decentlabs.io>
2022-06-24 23:01:06 -0400

    Remove default react UI, add some container margins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        md: '3rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
     extend: {},
   },
   plugins: [],
