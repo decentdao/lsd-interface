7b30b955ef22132df534984df9e801ea10592c65
Adam Gall <adam@decentlabs.io>
2022-06-25 10:47:45 -0400

    Wrap app in Rainbowkit provider

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,45 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider } = configureChains(
+  [chain.goerli],
+  [
+    publicProvider(),
+  ]
+);
+const { connectors } = getDefaultWallets({
+  appName: 'Liquid Streaming Derivatives',
+  chains
+});
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
   </React.StrictMode>
 );
 
