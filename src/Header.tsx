952630a78b0054c7da2904194d0da9c0cbac64ab
Adam Gall <adam@decentlabs.io>
2022-06-25 11:23:31 -0400

    Make header title link back to home

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,9 +1,14 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <div className="mb-4 py-4 border-b flex justify-between items-start">
-      <div className="text-4xl">Liquid Streaming Derivatives</div>
+      <div className="text-4xl">
+        <Link to="/">
+          🫠 Liquid Streaming Derivatives
+        </Link>
+      </div>
       <ConnectButton />
     </div>
   );
